9b15aab9572262016aef7385105b7da88dffb1e0
FE Nanodegree <tjallen1103@gmail.com>
2016-01-01 12:40:03 -0800

    Added js to remove render blocking css

--- dist/js/perfmatters.js
+++ dist/js/perfmatters.js
@@ -1,2 +1,2 @@
-/*! FEND-P4-Optimization 15y-12-31 */
+/*! FEND-P4-Optimization 16y-01-01 */
 function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,c=a.domComplete-a.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(a){logCRP()});
